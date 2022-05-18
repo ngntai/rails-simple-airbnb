02a57046bb7956c43ac43241d55d5253d401c42b
Tai Nguyen <ngn.tai@gmail.com>
2022-05-18 16:46:41 +0200

    Airbnb OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating 10 flats..."
+
+10.times do
+  flat = Flat.new(
+    name: Faker::TvShows::Simpsons.location,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Company.catch_phrase,
+    price_per_night: rand(1..500),
+    number_of_guests: rand(1..6)
+  )
+  flat.save!
+end
+puts 'Finished!'
